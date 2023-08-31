38848a1d74d1a25c441d25edba3b4b1d9bf6b479
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-08-31 11:42:33 +0330

    Transitioned to Redis async

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 import logging
 
 import aiocron
-import redis
+import redis.asyncio as redis
 from decouple import config
 from telethon import TelegramClient, events
 from telethon import functions
@@ -11,9 +11,9 @@ from telethon import functions
 telegram_id = 777000
 
 try:
-    r = redis.StrictRedis(host="localhost", port=6379, db=5, password="", decode_responses=True)
-except:
-    print("[-] i can`t connect to redis!")
+    r = redis.StrictRedis(host="localhost", port=6379, db=1, password="", decode_responses=True)
+except Exception as e:
+    print("[-] i can`t connect to redis!\n[-] error: " + str(e))
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s ',
                     level=logging.INFO)
@@ -38,7 +38,7 @@ client.start()
 
 @aiocron.crontab('*/1 * * * *')
 async def worker():
-    online_mode = r.get("online")
+    online_mode = await r.get("online")
 
     if online_mode == "1":
         await client(functions.account.UpdateStatusRequest(offline=False))
@@ -51,13 +51,13 @@ async def main(event):
 
 @client.on(events.NewMessage(from_users=admin_list, pattern="-online"))
 async def set_online(event):
-    r.set("online", "1")
+    await r.set("online", "1")
     await client.send_message(event.chat_id, "-Keep online mode turned on")
 
 
 @client.on(events.NewMessage(from_users=admin_list, pattern="-offline"))
 async def set_offline(event):
-    r.set("online", "0")
+    await r.set("online", "0")
     await client.send_message(event.chat_id, "-Keep online mode turned off")
 
 
