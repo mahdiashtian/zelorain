cad8faf09cd27054595983b9c7b939df4eb9203b
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-08-31 16:00:28 +0330

    Change fonts root

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def image_set_clock(time):
     image.load()
     W, H = image.size
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype(font="assets/fonts/Hacked-KerX.ttf", size=220)
+    font = ImageFont.truetype(font="assets/fonts/Hacked.ttf", size=220)
     left, top, right, bottom = font.getbbox(text=time)
     draw.text(((W - right) / 2, (H - bottom) / 2 - 18), time, font=font, fill="#ffffff")
     image.save(time_avatar)
