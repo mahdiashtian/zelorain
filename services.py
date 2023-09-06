403fb2f8b671aa003093cf673fed65e46468a514
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-09-06 13:32:24 +0330

    Added the function of deleting the sender's message (if the sender was the original account)

--- services.py
+++ services.py
@@ -4,3 +4,10 @@ from telethon.tl.functions.photos import DeletePhotosRequest
 async def delete_profile_photo(client, limit=None):
     me = await client.get_profile_photos("me", limit=limit)
     await client(DeletePhotosRequest(me))
+
+
+async def delete_sender_message(client, event):
+    sender = await event.get_sender()
+    me = await client.get_me()
+    if me.id == sender.id:
+        await client.delete_messages(event.chat_id, event.message.id)
